cbd3d69e4ea3ce6662d1e3b76db1411ba49a7824
Hxpnot1c <81385121+Hxpnot1c@users.noreply.github.com>
2024-03-03 00:43:19 +0000

    Removed unnecessary comments and added new comments

--- Testing/Data_Collection.py
+++ Testing/Data_Collection.py
@@ -19,24 +19,11 @@ def sample_data(channel=0):
             adc_value = prev_value
         yield adc_value
 
+# Computes mean of inputted NumPy array
 def mean(values):
     sum = np.sum(values)
     mean = sum / values.size
     return mean
-
-# Bins data using equal-width binning to achieve a regular sample rate of 1ksps (Using max heart sound freq of 400Hz, corresponding Nyquist rate is 0.8ksps. 0.2ksps is added to minimise data loss)
-# def equal_width_bins(values, timestamps, sampling_rate = 1000):
-#     sampling_period = 1 / sampling_rate
-#     binned_values = []
-#     binned_timestamps = []
-#     for bin_number in range(int(np.ceil(np.max(timestamps) / sampling_period))):
-#         # Computes mean amplitude value in the current bin_number (indexing from 0) with a width of sampling_period seconds
-#         bin_values = [values[x] for x in range(len(timestamps)) if timestamps[x] > bin_number * sampling_period and timestamps[x] <= (bin_number + 1) * sampling_period]
-#         bin = np.mean(bin_values)
-#         binned_values.append(bin)
-#         binned_timestamps.append(sampling_period * bin_number + (sampling_period * 0.5))
-#         print(bin_number)
-#     return binned_values, binned_timestamps
 
 values = np.array([])
 binned_values = np.array([])
@@ -45,16 +32,21 @@ sampling_rate = 1000
 sampling_period = 1 / sampling_rate
 sample_time = 10
 starttime = time.time()
+
+# Iterates through bins
 for bin in range(1, sample_time * 1000 + 1):
+    # Iterates through sample_data generator function for 1 sample_period seconds (1 millisecond)
     t_end = starttime + (bin * sampling_period)
-    # Iterates through generator function to create a list of values with their timestamps for a period of 10 seconds
     for adc_value in sample_data():
         if time.time() >= t_end:
             break
         values = np.append(values, adc_value)
+    # Computes mean of data from 1ms of sampling and appends it binned_values
     binned_values = np.append(binned_values, mean(values))
+    # Creates list of timestamps for each new sample of duration sampling_period
     binned_timestamps = np.append(binned_timestamps, sampling_period * bin - (sampling_period * 0.5))
-print(binned_values)
+
+# Plots the data and saves it to Digital-Stethoscope/Res/Data_Test
 plt.plot(binned_timestamps, binned_values, linestyle='solid', linewidth=0.4)
 plt.savefig('Res/Data_Test.png', bbox_inches='tight')
 plt.show()
