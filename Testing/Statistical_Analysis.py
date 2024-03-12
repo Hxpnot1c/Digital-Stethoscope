c1a52193ef98a1011585995539ab1bd6ab3a38c4
Hxpnot1c <81385121+Hxpnot1c@users.noreply.github.com>
2024-03-12 14:46:48 +0000

    Added bpm measuring

--- Testing/Statistical_Analysis.py
+++ Testing/Statistical_Analysis.py
@@ -19,6 +19,7 @@ def mean(values):
     sum = np.sum(values)
     mean = sum / len(values)
     return mean
+
 def remap_range(values, org_min, org_max, new_min, new_max):
     org_range = org_max - org_min
     new_range = new_max - new_min
@@ -26,26 +27,54 @@ def remap_range(values, org_min, org_max, new_min, new_max):
     remapped_values = (normalised_values * new_range) + new_min
     return remapped_values
 
-values, binned_values, binned_timestamps = [], [], []
-samples = 0
+def cycle_forward(new_val, t1, t2, t3, t4, t5):
+    t5 = t4
+    t4 = t3
+    t3 = t2
+    t2 = t1
+    t1 = new_val
+    return t1, t2, t3, t4, t5
+
+def calculate_heart_rate(t1, t2, t3, t4, t5):
+    bps = (5/t1 + 4/t2 + 4/t3 + 3/t4 + 2/t5) / 18
+    bpm = bps * 60
+    return bpm
+
+values, binned_values, binned_timestamps, bpm_sample_times = [], [], [], []
 sampling_rate = 1000
 sample_period = 1 / sampling_rate
-sampling_time = 10
+sampling_time = 20
+bpm_sample_num = 11 + 2
 
 print('Sampling...')
 starttime = time.perf_counter()
+next_bpm_sample_time = 0
 # Iterates through bins
 for bin in range(1, sampling_time * sampling_rate + 1):
     # Collects data samples using sample_data() function for 1 sample_period seconds (1 millisecond)
     t_end = starttime + (bin * sample_period)
-    while time.perf_counter() < t_end:
+    #TODO fix heart rate calulation
+    while (time.perf_counter()) < t_end:
         values.append(mcp.read_adc(0))
     # Computes mean of data from 1ms of sampling and appends it binned_values
-    binned_values.append(mean(values))
-    samples += len(values)
+    binned_values.append(mean_val := mean(values))
     values = []
     # Creates list of timestamps for each new sample of duration sample_period
-    binned_timestamps.append(sample_period * bin - (sample_period * 0.5))
+    binned_timestamps.append(current_time := sample_period * bin - (sample_period * 0.5))
+    if mean_val >= 700 and current_time >= next_bpm_sample_time:
+        bpm_sample_times.append(current_time)
+        next_bpm_sample_time = current_time + 0.2
+        if len(bpm_sample_times) == 11:
+            t1 = bpm_sample_times[-1] - bpm_sample_times[-3]
+            t2 = bpm_sample_times[-3] - bpm_sample_times[-5]
+            t3 = bpm_sample_times[-5] - bpm_sample_times[-7]
+            t4 = bpm_sample_times[-7] - bpm_sample_times[-9]
+            t5 = bpm_sample_times[-9] - bpm_sample_times[-11]
+            print(calculate_heart_rate(t1, t2, t3, t4, t5))
+        elif len(bpm_sample_times) == bpm_sample_num:
+            bpm_sample_num += 2
+            t1, t2, t3, t4, t5 = cycle_forward(bpm_sample_times[-1] - bpm_sample_times[-3], t1, t2, t3, t4, t5)
+            print(calculate_heart_rate(t1, t2, t3, t4, t5))
 print('Sampling complete!')
 timestamps = binned_timestamps
 input_signal = binned_values
